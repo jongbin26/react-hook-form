85d0b834abab198b8529dc9f7c195c3d250664cd
jongbin26 <010jjj@naver.com>
2024-10-10 22:21:44 +0900

    feat: test Form 작성

--- src/components/Form.tsx
+++ src/components/Form.tsx
@@ -1,5 +1,30 @@
+import { useForm, SubmitHandler } from 'react-hook-form';
+
+enum GenderEnum {
+	female = 'female',
+	male = 'male',
+	other = 'other',
+}
+
+interface IFormInput {
+	firstName: string;
+	gender: GenderEnum;
+}
 const Form = () => {
-  return <div>hello</div>;
+	const { register, handleSubmit } = useForm<IFormInput>();
+	const onSubmit: SubmitHandler<IFormInput> = (data) => console.log(data);
+
+	return (
+		<form onSubmit={handleSubmit(onSubmit)}>
+			<input {...register('firstName')} />
+			<select {...register('gender')}>
+				<option value="female">female</option>
+				<option value="male">male</option>
+				<option value="other">other</option>
+			</select>
+			<input type="submit" />
+		</form>
+	);
 };
 
 export default Form;
